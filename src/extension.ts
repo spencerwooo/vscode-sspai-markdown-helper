722e74fa497e3968ca06e8550897030cc77a698e
spencerwooo <spencerwoo98@foxmail.com>
2019-02-10 18:39:29 +0800

    🎁 Add main extension entry

--- src/extension.ts
+++ src/extension.ts
@@ -1,27 +1,95 @@
 // The module 'vscode' contains the VS Code extensibility API
 // Import the module and reference it with the alias vscode in your code below
+import * as fs from 'fs';
+import * as path from 'path';
+
 import * as vscode from 'vscode';
+import * as mkdirp from 'mkdirp';
 
 // this method is called when your extension is activated
 // your extension is activated the very first time the command is executed
 export function activate(context: vscode.ExtensionContext) {
 
-	// Use the console to output diagnostic information (console.log) and errors (console.error)
 	// This line of code will only be executed once when your extension is activated
-		console.log('Congratulations, your extension "vscode-sspai-markdown-style" is now active!');
+	console.log('Congratulations, your extension "vscode-sspai-markdown-helper" is now active!');
 
-	// The command has been defined in the package.json file
-	// Now provide the implementation of the command with registerCommand
-	// The commandId parameter must match the command field in package.json
-	let disposable = vscode.commands.registerCommand('extension.helloWorld', () => {
-		// The code you place here will be executed every time your command is executed
+	let disposable = vscode.commands.registerCommand('sspai.newPost', () => {
+		if (!vscode.workspace.rootPath) {
+			vscode.window.showErrorMessage('No project open.');
+			return;
+		}
 
-		// Display a message box to the user
-		vscode.window.showInformationMessage('Hello World!');
+		const options = {
+			folder: '',
+			post: '',
+			title: ''
+		};
+
+		vscode.window.showInputBox({
+			prompt: 'Which folder do you want to keep the post?',
+			placeHolder: ''
+		}).then((folder) => {
+			if (!folder) {
+				vscode.window.showErrorMessage('No folder name entered');
+				return;
+			}
+			options.folder = folder;
+			return vscode.window.showInputBox({
+				prompt: 'What is your file name?',
+				placeHolder: ''
+			});
+		}).then((post) => {
+			if (!post) {
+				vscode.window.showErrorMessage('No post name entered');
+				return;
+			}
+			options.post = post;
+			return vscode.window.showInputBox({
+				prompt: 'What is your post\'s title?',
+				placeHolder: ''
+			});
+		}).then((title) => {
+			if (!title) {
+				vscode.window.showErrorMessage('No title entered');
+				return;
+			}
+			options.title = title;
+
+			// rootPath returns a string or undefined, if VSCode is not in folder mode
+			let workspaceRoot = vscode.workspace.rootPath || '';
+
+			let imagePath = path.join(workspaceRoot, options.folder, 'image');
+			let postPath = path.join(workspaceRoot, options.folder, options.post + '.md');
+			let postContent = "<!-- Put your banner image on top of the post. -->\n"
+				+ "![](image/banner.png)\n"
+				+ "<!-- Replace it with your own banner. -->\n"
+				+ "# " + options.title;
+
+			mkdirp(imagePath, (err) => {
+				if (err) {
+					console.error(err);
+					vscode.window.showErrorMessage('Error! ' + err);
+				} else {
+					fs.writeFile(postPath, postContent, (err) => {
+						if (err) {
+							console.error(err);
+							vscode.window.showErrorMessage('Error! ' + err);
+						} else {
+							vscode.window.showInformationMessage('Post successfully generated!', 'OK');
+
+							let openPostPath = vscode.Uri.file(postPath);
+							vscode.workspace.openTextDocument(openPostPath).then(postDoc => {
+								vscode.window.showTextDocument(postDoc);
+							});
+						}
+					});
+				}
+			});
+		});
 	});
 
 	context.subscriptions.push(disposable);
 }
 
 // this method is called when your extension is deactivated
-export function deactivate() {}
+export function deactivate() { }
